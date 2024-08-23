16ed4d9a8e15415cdf90f22a456f173f5d305f59
Prudviraj <prudvivemula10@mail.com>
2024-08-23 16:03:32 +0530

    added converstation scprit

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: hii prudvi, how are you?"
+echo "prudvi:: hii ramesh, im fine. how are you"
+echo "ramesh:: what are you doing"
+echo "prudvi:: im learning devops. and you?"

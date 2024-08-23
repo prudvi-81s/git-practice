b6251c709feb676212363094bd64263484d6e74c
Prudviraj <prudvivemula10@mail.com>
2024-08-23 16:56:44 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hii $PERSON2, how are you?"
+echo "$PERSON2:: hii $PERSON1, im fine. how are you"
+echo "$PERSON1:: what are you doing"
+echo "$PERSON2:: im learning devops. and you?"

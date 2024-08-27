27c65565d41daf6408987b70356319195e35089e
Prudviraj <prudvivemula10@mail.com>
2024-08-27 21:28:20 +0530

    shell

--- 13-loops.sh
+++ 13-loops.sh
@@ -27,5 +27,14 @@ CHECK_ROOT
 
 for package in $@ 
 do 
-    echo $packages
+  dnf list installed $package
+  if [$? -ne 0]
+  then
+    echo "$package is not installed, going to install it.."
+    dnf install $package-y
+    VALIDATE $? "installing $package"
+  else
+    echo "$package is already installed..nothing to do"
+  fi
 done
+
